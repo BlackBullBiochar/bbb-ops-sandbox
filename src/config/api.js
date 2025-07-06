402025350d5214491328de204457a817a7cf53fa
George Ferguson <george@blackbullbiochar.com>
2025-07-06 15:02:22 +0100

    testing netlify

--- src/config/api.js
+++ src/config/api.js
@@ -6,7 +6,7 @@ const getAPI = () => {
   if (isLocalhost) return "http://localhost:4000";
   if (isNgrok) return "https://98bb-2a01-4b00-ab31-d800-9144-c3df-b05f-a996.ngrok-free.app";
 
-  return import.meta.env.VITE_API_URL || "https://your-future-prod-url.com";
+  return import.meta.env.VITE_API_URL || "https://98bb-2a01-4b00-ab31-d800-9144-c3df-b05f-a996.ngrok-free.app";
 };
 
 export const API = getAPI();
