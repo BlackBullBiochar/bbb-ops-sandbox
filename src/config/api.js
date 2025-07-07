3bdf0b7020fd62c3f6e6fa2484c82ddd0fed2ac3
George Ferguson <george@blackbullbiochar.com>
2025-07-07 02:39:31 +0100

    new static  ngrok

--- src/config/api.js
+++ src/config/api.js
@@ -7,10 +7,10 @@ const getAPI = () => {
   const isStaging = window.location.href.includes("staging");
 
   if (isLocalhost) return "http://localhost:4000";
-  if (isNgrok) return "https://relevant-feline-equal.ngrok-free.app ";
-  if (isStaging) return "https://relevant-feline-equal.ngrok-free.app ";
+  if (isNgrok) return "https://relevant-feline-equal.ngrok-free.app";
+  if (isStaging) return "https://relevant-feline-equal.ngrok-free.app";
 
-  return "https://relevant-feline-equal.ngrok-free.app ";
+  return "https://relevant-feline-equal.ngrok-free.app";
 };
 
 export const API = getAPI();
