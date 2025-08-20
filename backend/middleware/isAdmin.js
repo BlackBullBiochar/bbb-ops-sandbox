bf4cfe7918ac419f2e228d6ae51b4abd20fe7632
George Ferguson <george@blackbullbiochar.com>
2025-08-20 15:02:30 +0100

    removing frontend backend and dist

--- backend/middleware/isAdmin.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-  if (!req.tokenData?.isAdmin) {
-    return res.status(403).json({ message: 'Admins only' });
-  }
-  next();
-};
-
-module.exports = { isAdmin };
