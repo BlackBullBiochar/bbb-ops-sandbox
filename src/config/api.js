a3b78f9db7c13b8bc88ec62d9b84a335787f379f
George Ferguson <george@blackbullbiochar.com>
2025-09-03 17:04:02 +0100

    test went well returned to regular api

--- src/config/api.js
+++ src/config/api.js
@@ -6,9 +6,9 @@ const getAPI = () => {
   const isNgrok = window.location.href.includes("ngrok");
   const isStaging = window.location.href.includes("staging");
 
-  if (isLocalhost) return "https://84ngkrzw-4000.uks1.devtunnels.ms";
+  if (isLocalhost) return "http://localhost:4000";
   if (isNgrok) return "https://relevant-feline-equal.ngrok-free.app";
-  if (isStaging) return "https://84ngkrzw-4000.uks1.devtunnels.ms";
+  if (isStaging) return "https://bbb-staging-ae2bb81703e0.herokuapp.com";
 
   return "https://api.blackbullbiochar.com";
 };
