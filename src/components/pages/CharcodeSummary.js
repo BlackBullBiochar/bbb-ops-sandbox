e5465f98fdbae7400f8751b69fb35f3b37257a1e
George Ferguson <george@blackbullbiochar.com>
2025-09-09 20:48:43 +0100

    another slight tweak

--- src/components/pages/CharcodeSummary.js
+++ src/components/pages/CharcodeSummary.js
@@ -145,8 +145,8 @@ const CharcodeSummaryView = () => {
       ? "No bags were logged this week"
       : "Bags logged more than 3 days after production";
 
-  const JNRCountPlaceholder = searched && JNRcount === 0 && ARAPercent === "" ? "" : JNRcount;
-  const ARACountPlaceholder = searched && ARAcount === 0 && JNRPercent === ""? "" : ARAcount;
+  const JNRCountPlaceholder = searched && JNRcount === 0 && JNRPercent === "" ? "" : JNRcount;
+  const ARACountPlaceholder = searched && ARAcount === 0 && ARAPercent === ""? "" : ARAcount;
 
   const ARAPercentBlurb =
     searched && (araCounts?.bagging || 0) === 0
